42f3387f397751f174dc8d545113dad8316b6680
Tasuku Bobcorn <DevBobcorn@outlook.com>
2023-04-19 23:02:56 +0800

    :bento: Tweak colors

--- Assets/Scripts/Mapping/World.cs
+++ Assets/Scripts/Mapping/World.cs
@@ -8,11 +8,9 @@ namespace MarkovCraft.Mapping
     /// </summary>
     public class World
     {
-        // Using biome colors of minecraft:plains as default
-        // See https://minecraft.fandom.com/wiki/Plains
-        public static readonly float3 DEFAULT_FOLIAGE = new float3(119, 171, 47) / 255F;
-        public static readonly float3 DEFAULT_GRASS   = new float3(145, 189, 89) / 255F;
-        public static readonly float3 DEFAULT_WATER   = new float3(63, 118, 228) / 255F;
+        public static readonly float3 DEFAULT_FOLIAGE = new float3(119, 255,  47) / 255F;
+        public static readonly float3 DEFAULT_GRASS   = new float3( 95, 255,  39) / 255F;
+        public static readonly float3 DEFAULT_WATER   = new float3( 63, 118, 228) / 255F;
 
         public float3 GetFoliageColor(Location loc) => DEFAULT_FOLIAGE;
 
